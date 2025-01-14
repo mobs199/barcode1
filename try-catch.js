55ecab63f55886a6f4eb58ae9ef04e2fcee9e32e
mobs199 <beshtawi@ewanto.de>
2025-01-14 10:54:50 +0000

    .

---
+++ try-catch.js
@@ -0,0 +1,31 @@
+let fetchRetourData = async function (sendungsnummer) {
+    const url = `/retoure/lookup/barcode=${sendungsnummer}&type=qrcode`;
+  
+    try {
+      const response = await fetch(url);
+  
+      if (!response.ok) {
+        throw new Error(`Error: ${response.status} ${response.statusText}`);
+      }
+  
+      const data = await response.json();
+      console.log('Success:', data);
+  
+      let liefernummer = data.liefernummer;
+      let versandart = data.versandart;
+      let kundePlz = data.kunde_plz;
+      let kundeName = data.kunde_name;
+      let orderNumber = data.orderNumber;
+  
+      console.log('Liefernummer:', liefernummer);
+      console.log('Versandart:', versandart);
+      console.log('Kunden-PLZ:', kundePlz);
+      console.log('Kundenname:', kundeName);
+      console.log('orderNumber:', orderNumber);
+  
+    } catch (error) {
+      console.error('An error occurred:', error.message);
+      alert(`An error occurred: ${error.message}`);
+    }
+  };
+  
